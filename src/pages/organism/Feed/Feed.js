9584b45afcc4370fd04edb00b1aa0871b29bfc57
acepnurmansidik <acepnurmansidik@gmail.com>
2022-01-21 09:44:52 +0700

    adding btns at feed&profile people

--- src/pages/organism/Feed/Feed.js
+++ src/pages/organism/Feed/Feed.js
@@ -193,7 +193,71 @@ function MyVerticallyCenteredModal(props) {
                   <p>Hello lisa, today i'll eat you at your badroom.</p>
                 </div>
               </div>
+              <div href="/people" className="modal-card-people">
+                <Link to="/profile-people" className="modal-card-img">
+                  <img src="/assets/img/Rectangle 8.png" alt="" />
+                </Link>
+                <div className="modal-info-people">
+                  <label for="" className="mt-3">
+                    Acep
+                  </label>
+                  <p>Hello lisa, today i'll eat you at your badroom.</p>
+                </div>
+              </div>
+              <div href="/people" className="modal-card-people">
+                <Link to="/profile-people" className="modal-card-img">
+                  <img src="/assets/img/Rectangle 8.png" alt="" />
+                </Link>
+                <div className="modal-info-people">
+                  <label for="" className="mt-3">
+                    Acep
+                  </label>
+                  <p>Hello lisa, today i'll eat you at your badroom.</p>
+                </div>
+              </div>
+              <div href="/people" className="modal-card-people">
+                <Link to="/profile-people" className="modal-card-img">
+                  <img src="/assets/img/Rectangle 8.png" alt="" />
+                </Link>
+                <div className="modal-info-people">
+                  <label for="" className="mt-3">
+                    Acep
+                  </label>
+                  <p>Hello lisa, today i'll eat you at your badroom.</p>
+                </div>
+              </div>
+              <div href="/people" className="modal-card-people">
+                <Link to="/profile-people" className="modal-card-img">
+                  <img src="/assets/img/Rectangle 8.png" alt="" />
+                </Link>
+                <div className="modal-info-people">
+                  <label for="" className="mt-3">
+                    Acep
+                  </label>
+                  <p>Hello lisa, today i'll eat you at your badroom.</p>
+                </div>
+              </div>
             </div>
+            <Card.Body>
+              <div className="info-statusRell-modal">
+                <div className="statusRell-nav-btnModal">
+                  <div className="statusRell-nav-body">
+                    <div className="statusRell-nav-btn">
+                      <Link to="/feed">
+                        <img src="../assets/icons/love.svg" alt="" />
+                      </Link>
+                      <Link to="/feed">
+                        <img src="../assets/icons/comment.svg" alt="" />
+                      </Link>
+                      <Link to="/feed">
+                        <img src="../assets/icons/share.svg" alt="" />
+                      </Link>
+                    </div>
+                  </div>
+                  <p>1.234 Like</p>
+                </div>
+              </div>
+            </Card.Body>
           </div>
         </div>
       </Modal.Body>
